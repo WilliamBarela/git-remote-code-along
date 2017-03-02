8f506f08a8ef7c5ab5bb319470523cc7bdfeacbf
William Barela <william.barela.dev@gmail.com>
2017-03-02 23:50:17 +0900

    added pythagorian

--- hello-world-git.py
+++ hello-world-git.py
@@ -1,8 +1,14 @@
+from math import sqrt
+
 def adder(x, y):
 	return x + y
 
 def hello_git(name):
 	return "hello" + name
+	
+def pythag(a, b):
+	return math.sqrt(a**2 + b**2)
 
 adder(3, 4)
 hello_git("git")	
+pythag(3, 4)
